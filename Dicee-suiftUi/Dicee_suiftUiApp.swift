c03ab263ebf0c00d1ecaa6dc43cda2fd357378cd
Amit Singh <amitsighbxr@gmail.com>
2023-09-07 17:39:00 +0800

    Initial Commit

---
+++ Dicee-suiftUi/Dicee_suiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_suiftUiApp.swift
+//  Dicee-suiftUi
+//
+//  Created by user243065 on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_suiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
